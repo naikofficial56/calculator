9dc985b93f0e885cd94d8b1bef403450c9ad2e2f
Kartik Naik <naikofficial56@gmail.com>
2022-06-12 21:25:02 +0530

    first commit

---
+++ package/utilies.go
@@ -0,0 +1,17 @@
+package calculator
+
+func sum(a int, b int) int {
+	return a + b
+}
+
+func multiply(a int, b int) int {
+	return a * b
+}
+
+func substract(a int, b int) int {
+	return a - b
+}
+
+func divide(a int, b int) int {
+	return a / b
+}
---
+++ package/utilities_test.go
@@ -0,0 +1,33 @@
+package calculator
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	type testCases struct {
+		name     string
+		input1   int
+		input2   int
+		expected int
+	}
+	testCasesAll := []testCases{
+		{
+			name:     "correct output",
+			input1:   5,
+			input2:   10,
+			expected: 15,
+		},
+		{
+			name:     "incorrect output",
+			input1:   5,
+			input2:   11,
+			expected: 16,
+		},
+	}
+
+	for _, testCase := range testCasesAll {
+		result := sum(testCase.input1, testCase.input2)
+		if result != testCase.expected {
+			t.Errorf("expected = %d, but got = %d", testCase.expected, result)
+		}
+	}
+}
